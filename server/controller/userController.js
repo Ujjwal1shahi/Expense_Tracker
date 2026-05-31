8d644772fe76dd6ec9497cb0b16f91a70694b4bf
Ujjwal1shahi <ujjwalshahi2306@gmail.com>
2026-05-31 19:01:30 +0530

    added authController and  userController and updated routes

---
+++ server/controller/userController.js
@@ -0,0 +1,35 @@
+export const getUser = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        req.status(500).json({
+            status: "failed",
+            message: error.message
+        });
+    }
+};
+
+export const changePassword = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        req.status(500).json({
+            status: "failed",
+            message: error.message
+        });
+    }
+};
+
+export const updateUser = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        req.status(500).json({
+            status: "failed",
+            message: error.message
+        });
+    }
+};
